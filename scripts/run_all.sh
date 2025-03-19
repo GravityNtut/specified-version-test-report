26327ce20b31a12dc513987106e5878c2fde179d
tako_selab <t112598023@ntut.org.tw>
2025-03-19 15:17:39 +0800

    fix: change run branch and rewrite shell

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -4,13 +4,12 @@
 # $2 gravity_dispatcher_version
 # $3 gravity_sdk_version
 
-# run gravity-cli-tests test
-mkdir -p test_code
-rm -rf test_code/*
+root_path=$(pwd)
+
 cd test_code
 
-# TODO: 改main branch
-git clone https://github.com/BrobridgeOrg/gravity-cli-tests.git --branch GN-206_specified_version_test
+# run gravity-cli-tests test
 cd gravity-cli-tests
-# TODO: 改shell參數透過action傳入
 earthly -P +specified-version-test --nats_jetstream_version=$1 --gravity_dispatcher_version=$2 --gravity_sdk_version=$3
+
+cd $root_path
