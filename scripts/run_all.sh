c87e35635f547871867d28acf8e56e70d6e8f692
tako_selab <t112598023@ntut.org.tw>
2025-03-19 11:43:50 +0800

    fix: make shell use input

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# $1 nats_jetstream_version
+# $2 gravity_dispatcher_version
+# $3 gravity_sdk_version
+
 # run gravity-cli-tests test
 mkdir -p test_code
 rm -rf test_code/*
@@ -9,4 +13,4 @@ cd test_code
 git clone https://github.com/BrobridgeOrg/gravity-cli-tests.git --branch GN-206_specified_version_test
 cd gravity-cli-tests
 # TODO: 改shell參數透過action傳入
-earthly -P +specified-version-test --gravity_sdk_version=v2.0.7 --nats_jetstream_version=v1.3.21-20250117 --gravity_dispatcher_version=v0.0.31-20250220
+earthly -P +specified-version-test --nats_jetstream_version=$1 --gravity_dispatcher_version=$2 --gravity_sdk_version=$3
