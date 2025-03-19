e3978d3c2f2fee6fd1a868bb66196b4970dc1b9f
tako_selab <t112598023@ntut.org.tw>
2025-03-19 11:33:43 +0800

    feat: add github workflow

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # run gravity-cli-tests test
-mkdir test_code
+mkdir -p test_code
 rm -rf test_code/*
 cd test_code
+
 # TODO: 改main branch
 git clone https://github.com/BrobridgeOrg/gravity-cli-tests.git --branch GN-206_specified_version_test
 cd gravity-cli-tests
-earthly -P +specified-version-test
-
-# TODO: 移除 改workflow觸發
-sh ./create_test_report.sh
+# TODO: 改shell參數透過action傳入
+earthly -P +specified-version-test --gravity_sdk_version=v2.0.7 --nats_jetstream_version=v1.3.21-20250117 --gravity_dispatcher_version=v0.0.31-20250220
